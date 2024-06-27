47c981355825e065941266f6ede49b4f18a7f97a
username <nurli50702@gmail.com>
2024-06-27 10:09:12 -0700

    update

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config():
 
     #for the computer accese or read path
     FLASK_APP = os.getenv('FLASK_APP')  
-    # FLASK_DEBUG =os.getenv('FLASK_DEBUG')
+    FLASK_DEBUG =os.getenv('FLASK_DEBUG')
 
     FLASK_ENV = os.getenv('FLASK_ENV')  
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'anyone will never get access to my CSS' 
@@ -20,4 +20,4 @@ class Config():
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICAITONS = False
-    # STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
+    STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
